Finished Gradient Ascent. Other minor fixes

gradientAscent.py
@@ -10,6 +10,9 @@ class GradientAscentLogReg:
     def fitLogReg(self, y, X):
         beta0 = 0
         beta = np.random.randn(X.shape[1], 1)
+        print('beta start')
+        print(beta)
+        print('--------')
         s = 1e-5
         i = 0
         MAXITER = 2000
@@ -18,7 +21,7 @@ class GradientAscentLogReg:
         lP = logProbability()
 
         while i < MAXITER:
-            print(i)
+            #print(i)
             beta0New, betaNew = lP.dLogLikLogReg(y, X, beta0, beta)
             beta0 += s * beta0New
             beta += s * betaNew
@@ -26,11 +29,23 @@ class GradientAscentLogReg:
 
         return beta0, beta
 
-print('!!!!!!!!!!')
-mat = loadmat('hw1.mat')
-#print(mat)
-#exit()
-X = np.array(mat['X']).T
-y = np.array(mat['y']).T
-gA = GradientAscentLogReg()
-gA.fitLogReg(y, X)
+def main():
+    print('Loading .mat file...')
+    mat = loadmat('hw1.mat')
+    X = np.array(mat['X']).T
+    y = np.array(mat['y']).T
+    print('Starting Gradient Ascent...')
+    gA = GradientAscentLogReg()
+    beta0, beta = gA.fitLogReg(y, X)
+    print('Finished. Found Solutions:')
+    print('beta0 = ')
+    print(beta0)
+    print('beta = ')
+    print(beta)
+    print('----------')
+    print(beta.T)
+    print('----------')
+    print(beta.flatten())
+
+if __name__ == "__main__":
+    main()

logProbability.py
@@ -28,33 +28,34 @@ class logProbability:
 
     def dLogLikLogReg(self, y, X, beta0, beta):
         dbeta = np.empty(beta.shape)
-        dbeta0 = sum((1 - 1 / (1 + np.exp(-y * (beta0 + np.dot(X, beta))))) * y)
+        dbeta0 = sum((1 - 1 / (1 + np.exp(-y * (beta0 + np.dot(X, beta))))) * y)[0]
         for p in range(beta.shape[0]):
             dbeta[p, 0] = sum((1 - 1 / (1 + np.exp(-y * (beta0 + np.dot(X, beta))))) * y * X[:, p][:, np.newaxis])
-        return dbeta0[0], dbeta
+        return dbeta0, dbeta
 
-#mat = sp.loadmat('hw1.mat')
-#y = np.random.randn(10, 1)
-#x = np.random.randn(10, 1)
-#beta0 = np.random.randn()
-#beta = np.random.randn(10, 1)
-#lP = logProbability()
-#print(lP.dLogLikLogReg(y, x, beta0, beta))
+if __name__ == "__main__":
+    mat = sp.loadmat('hw1.mat')
+    y = np.random.randn(10, 1)
+    x = np.random.randn(10, 1)
+    beta0 = np.random.randn()
+    beta = np.random.randn(10, 1)
+    lP = logProbability()
+    print(lP.dLogLikLogReg(y, x, beta0, beta))
 
-#print(mat['X'])
-#X = np.array(mat['X'])
-#y = np.array(mat['y'])
-#print(np.shape(y))
-#print(np.shape(X))
-#print(mat)
-#exit()
+    print(mat['X'])
+    X = np.array(mat['X'])
+    y = np.array(mat['y'])
+    print(np.shape(y))
+    print(np.shape(X))
+    print(mat)
+    exit()
 
-#for i in range(1000):
-#    lP = logProbability()
-#    x = np.random.randn(10, 1)
-#    beta0 = np.random.randn()
-#    beta = np.random.randn(10, 1)
-#    lP.predictY(x, beta0, beta)
-#    print('Solution: ')
-#    print(lP.predY)
-#    print(lP.logProbY)
+    for i in range(1000):
+        lP = logProbability()
+        x = np.random.randn(10, 1)
+        beta0 = np.random.randn()
+        beta = np.random.randn(10, 1)
+        lP.predictY(x, beta0, beta)
+        print('Solution: ')
+        print(lP.predY)
+        print(lP.logProbY)